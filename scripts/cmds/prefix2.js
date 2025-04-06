1d8c7a26d9b17802fec4a42d62e87546bc7789e8
tomt522 <rifatjamil613@gmail.com>
2025-04-06 21:50:03 +0600

    Update and rename prefix3.js to prefix2.js

--- scripts/cmds/prefix2.js
+++ scripts/cmds/prefix2.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-   name: "prefix3",
+   name: "prefix",
    version: "1.0",
    author: "Nyx",
    countDown: 5,
@@ -15,12 +15,12 @@ module.exports = {
        body: `Hello!  𝙃ō𝙩𝙖𝙧ō 𝙊𝙧𝙚𝙠𝙞🕸️
 🌟 **My Prefix:** [ - ]  
 📜 **How to get started:**  
-➡️ Type *help to view all available commands.  
+➡️ Type -help to view all available commands.  
 
 💬 **Need Help?**  
 👉 Feel free to ask!  
 
-🚀 Have a great day!\n Creator: https://www.facebook.com/share/19jKp8u7th/`, attachment: await global.utils.getStreamFromURL("https://i.imgur.com/UDTF8eN.mp4"),
+🚀 Have a great day!\n 
      });
    }
  }
